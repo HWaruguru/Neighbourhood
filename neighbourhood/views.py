27ea61fa40f4184350a8eac1c39927a96e45b44f
HWaruguru <hannah.waruguru8@gmasil.com>
2021-11-03 08:58:45 +0300

    add signup function

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,7 +1,24 @@
 from django.conf.urls import url,include
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+from neighbourhood.forms import SignupForm
 
 
 def index(request):
     return render(request, 'index.html')
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = AuthenticationForm(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
